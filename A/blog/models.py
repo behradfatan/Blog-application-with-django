46070d1ce95aef5733f5388e9caf7086a815babf
behradfatan <behrad_fatan@yahoo.com>
2021-09-08 14:47:24 +0430

    complete the blog models

--- A/blog/models.py
+++ A/blog/models.py
@@ -1,6 +1,21 @@
 from django.db import models
+# import datetime
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Article(models.Model):
+    STATUS = (
+        ('draft', 'Draft'),
+        ('publish', 'Publish')
+    )
     title = models.CharField(max_length=120)
-    slug = models.SlugField(max_length=120)
+    slug = models.SlugField(max_length=120, unique=True)
+    writer = models.ForeignKey(User, on_delete=models.CASCADE, default='')
+    body = models.TextField(default=None)
+    # default = datetime.datetime.now
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(default=timezone.now)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=20, choices=STATUS, default='draft')
+
