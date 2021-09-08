fbd40d5584a3b1d69637b0464941624ec870af06
behradfatan <behrad_fatan@yahoo.com>
2021-09-08 16:54:06 +0430

    create get_abolute_url in models and create a new view

--- A/blog/models.py
+++ A/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 # import datetime
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class Article(models.Model):
@@ -18,4 +19,10 @@ class Article(models.Model):
     created = models.DateTimeField(default=timezone.now)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=20, choices=STATUS, default='draft')
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('article_detail', args=[self.id, self.slug])
 
--- A/blog/views.py
+++ A/blog/views.py
@@ -5,3 +5,7 @@ from .models import Article
 def all_articles(request):
     all_articles = Article.objects.all()
     return render(request, "all_articles.html", {"all_articles": all_articles})
+
+def article_detail(request, id, slug):
+    article = Article.objects.get(id=id, slug=slug)
+    return render(request, 'article.html', {"article": article})
