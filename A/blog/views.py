156ddabba38c5dc54bb0d36db1ef610686d721f2
behradfatan <behrad_fatan@yahoo.com>
2021-09-08 17:22:12 +0430

    handle with error 404

--- A/blog/views.py
+++ A/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Article
 
 
@@ -7,5 +7,6 @@ def all_articles(request):
     return render(request, "all_articles.html", {"all_articles": all_articles})
 
 def article_detail(request, id, slug):
-    article = Article.objects.get(id=id, slug=slug)
+    # article = Article.objects.get(id=id, slug=slug)
+    article = get_object_or_404(Article, id=id, slug=slug)
     return render(request, 'article.html', {"article": article})
