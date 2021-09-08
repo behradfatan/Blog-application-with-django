b355684660ca92533b9b46a928acbb8cac029817
behradfatan <behrad_fatan@yahoo.com>
2021-09-08 15:45:44 +0430

    create templates and views and test first page

--- A/blog/views.py
+++ A/blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Article
 
-# Create your views here.
+
+def all_articles(request):
+    all_articles = Article.objects.all()
+    return render(request, "all_articles.html", {"all_articles": all_articles})
