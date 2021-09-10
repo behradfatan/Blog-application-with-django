73ee2123a9bfaa7ff5e9f07ed12a8ba4daffd5bb
behradfatan <behrad_fatan@yahoo.com>
2021-09-10 17:16:41 +0430

    use message frame work to show message when user is login or the username or pass is incorrect and use extra tags to show better

--- A/accounts/views.py
+++ A/accounts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import UserLoginForm
 from django.contrib.auth import authenticate, login
+from django.contrib import messages
+
 
 
 def user_login(request):
@@ -13,7 +15,10 @@ def user_login(request):
             user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
+                messages.success(request, 'you logged in successfully', 'success')
                 return redirect('all_articles')
+            else:
+                messages.error(request, 'wrong username or password!!!', 'warning')
     else:
         forms = UserLoginForm()
     return render(request, 'user_login.html', {"forms": forms})
