5226b4687edb4ffcbcc557863cae0694369fd205
behradfatan <behrad_fatan@yahoo.com>
2021-09-11 10:03:18 +0430

    customizing form validation for email field

--- A/accounts/forms.py
+++ A/accounts/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-
+from django.contrib.auth.models import User
 
 class UserLoginForm(forms.Form):
     username = forms.CharField(max_length=20, widget=forms.TextInput(attrs={"class": "form-control"}))
@@ -10,6 +10,13 @@ class UserRegister(forms.Form):
     username = forms.CharField(max_length=20, widget=forms.TextInput(attrs={"class": "form-control"}))
     password = forms.CharField(max_length=50, widget=forms.PasswordInput(attrs={"class": "form-control"}))
     email = forms.EmailField(max_length=30, widget=forms.EmailInput(attrs={"class": "form-control"}))
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        user = User.objects.filter(email=email)
+        if user.exists():
+            raise forms.ValidationError("your email already exist")
+        return email
 
 
 
