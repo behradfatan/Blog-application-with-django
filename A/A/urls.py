5965c6934361ca1e2005a4b690c7c88b22141116
behradfatan <behrad_fatan@yahoo.com>
2021-09-11 12:11:38 +0430

    use ckeditor uploader and change body field for upload image

--- A/A/urls.py
+++ A/A/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('account/', include('accounts.urls')),
+    path('ckeditor_upload/', include('ckeditor_uploader.urls')),
 ]
--- A/blog/models.py
+++ A/blog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
-from ckeditor.fields import RichTextField
+from ckeditor_uploader.fields import RichTextUploadingField
 
 
 class Article(models.Model):
@@ -14,7 +14,7 @@ class Article(models.Model):
     title = models.CharField(max_length=120)
     slug = models.SlugField(max_length=120, unique=True)
     writer = models.ForeignKey(User, on_delete=models.CASCADE, default='')
-    body = RichTextField()
+    body = RichTextUploadingField()
     # default = datetime.datetime.now
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(default=timezone.now)
